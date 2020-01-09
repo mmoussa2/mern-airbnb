8b4873f7e3bccc9222950dd450a13220ab56998f
Jesse <jesse.mwarren@yahoo.com>
2020-01-09 10:28:02 -0800

    m

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from '../nav/navbar_container'
+import {Route} from 'react-router-dom'
 
 class MainPage extends React.Component {
 
   render() {
     return (
       <div>
-        <NavBar />
+        <Route path="/" component={NavBar} />
         <h1>An AirBnB Clone</h1>
         <footer>
           Copyright &copy; 2020 EndorBnB
