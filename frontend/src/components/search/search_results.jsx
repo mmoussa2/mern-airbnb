211e898da413f1b3dd791467531a4f0865d0f881
AlexanderDGeorge <alexander97.george@gmail.com>
2020-01-14 16:29:53 -0800

    push to heroku

--- frontend/src/components/search/search_results.jsx
+++ frontend/src/components/search/search_results.jsx
@@ -16,38 +16,38 @@ class NavBar extends React.Component {
   }
 
   render() {
-    let result = null
-    if (this.props.search !== "") {
-      result = (
+      return (
         <div className="search-results flex flex-wrap">
           {this.props.properties.map((property, index) => {
             return (
-              <div 
+              <div
                 id={property._id}
                 onClick={this.browseToProperty}
-                className="search-result flex column" 
-                key={index}>
+                className="search-result flex column"
+                key={index}
+              >
                 <div>
-                  <img className="search-result-image" src={property.imageUrl} />
+                  <img
+                    className="search-result-image"
+                    src={property.imageUrl}
+                  />
                 </div>
                 <div className="">
                   <div>Treehouse</div>&nbsp;
-                  <div>{property.beds > 1 ? property.beds + " beds" : property.beds + " bed" } </div>
+                  <div>
+                    {property.beds > 1
+                      ? property.beds + " beds"
+                      : property.beds + " bed"}{" "}
+                  </div>
                 </div>
                 <div>{property.title}</div>
                 <div>${property.price} / night</div>
                 <div>{property.location}</div>
-            </div>
-            )
+              </div>
+            );
           })}
         </div>
-      )
-    } else {
-      result = <div className="search-results">
-        no search
-      </div>
-    }
-    return result;
+      );
   }
 }
 
